e8f97dcd7583aa8790dae4213f77e18ac1ad7884
Alex Quennelle <BluePhoenixcg@gmail.com>
2024-10-29 22:52:49 -0500

    Fix build script

--- BuildWASM.bat
+++ BuildWASM.bat
@@ -1,1 +1,1 @@
-em++ -o cpp.js .\cpp\main.cpp -sEXPORTED_RUNTIME_METHODS=cwrap -sEXPORTED_FUNCTIONS="_Goodbye,_main"
+em++ -o cpp.js -O3 .\cpp\main.cpp -sEXPORTED_RUNTIME_METHODS=cwrap -sEXPORTED_FUNCTIONS="_Hash,_main"
